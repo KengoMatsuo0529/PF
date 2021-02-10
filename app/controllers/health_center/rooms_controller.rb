eb44264303844259dbc184559f1048fb176a9bbf
KengoMatsuo0529 <k.matsuo0529@gmail.com>
2021-02-10 01:37:16 +0000

    link-set

--- app/controllers/health_center/rooms_controller.rb
+++ app/controllers/health_center/rooms_controller.rb
@@ -1,2 +1,16 @@
 class HealthCenter::RoomsController < ApplicationController
+  
+  
+  def index
+    @rooms = Room.all.order(:id)
+  end
+  
+  def show
+    @room = current_health_center.rooms.find_by(user_id: params[:user_id])
+    unless @room
+      @room = current_health_center.rooms.create(user_id: params[:user_id])
+    end
+    @messages = Message.where(room_id: @room.id)
+  end
+  
 end
