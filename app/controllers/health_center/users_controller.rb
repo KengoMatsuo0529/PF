9521a8c76dccd7df4f5a918b452d048adfe8aa8b
KengoMatsuo0529 <k.matsuo0529@gmail.com>
2021-02-17 11:41:17 +0000

    health-center-fix

--- app/controllers/health_center/users_controller.rb
+++ app/controllers/health_center/users_controller.rb
@@ -1,10 +1,14 @@
 class HealthCenter::UsersController < ApplicationController
-  
+
   def index
-    @users = User.search(params[:search])
+    @messages = current_health_center.messages
+    user_ids = @messages.map(&:user_id)
+    @users = User.where(id: user_ids).search(params[:search])
+    # user_ids = @messages.map{|message| message.user_id}
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
 end
+
--- app/controllers/user/health_centers_controller.rb
+++ app/controllers/user/health_centers_controller.rb
@@ -13,11 +13,6 @@ class User::HealthCentersController < ApplicationController
     #   if Message.where(user_id: current_user.id).last.updated_at <= Message.where(health_center_id: @health_center.id).last.created_at
     #     @checked = true
     #   end
-
-      # Message.where(user_id: current_user.id).where(health_center_id: @health_center.id).last.checked == 0
-
-    # 日付の比較をして、自分のメッセージのupdated_atと、ルームにあるメッセージのupdated_atを<=等で比較する
-    # 表示させるのであれば、保健所のuserコントローラーにも同様に記載
   end
 
 
