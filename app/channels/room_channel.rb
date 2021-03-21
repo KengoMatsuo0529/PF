5997e09118150a078e42cdde3d681912586faf3c
KengoMatsuo0529 <k.matsuo0529@gmail.com>
2021-03-21 07:53:08 +0000

    [last]

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,3 +1,4 @@
+#サーバーサイドの処理を記載
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from "room_channel"
@@ -6,6 +7,7 @@ class RoomChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+  
   def speak(data)
     room = Room.find(data['room_id'])
     Message.create! message: data['message'], room_id: data['room_id'], checked: data['checked'], user_checked: false, healthcenter_checked: false, user_id: room.user_id, 
